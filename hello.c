b280f4f761890d8e8b943c456f6213d27797363c
John Ryan <jtigger@pm.me>
2024-12-26 11:29:35 -0800

    (WIP) extract Tree

--- hello.c
+++ hello.c
@@ -4,6 +4,16 @@
 #define CANVAS_HEIGHT 20
 #define CANVAS_WIDTH 60
 
+// Struct to represent a tree.
+typedef struct
+{
+    int x;
+    int y;
+    int height;
+    char leaf_char;
+} Tree;
+
+
 // Print the canvas to the console.
 void canvas_print(char canvas[CANVAS_HEIGHT][CANVAS_WIDTH]);
 
@@ -14,21 +24,26 @@ void canvas_wipe(char canvas[CANVAS_HEIGHT][CANVAS_WIDTH]);
 void canvas_write(char canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int rel_row, int rel_col, char c);
 
 // Write a tree to the canvas at the given height.
-void canvas_write_tree(char canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int x, int y, int height, char leaf_char);
+void canvas_write_tree(char canvas[CANVAS_HEIGHT][CANVAS_WIDTH], Tree tree);
 
 // Reposition the cursor to the top left corner of the canvas.
 void reset_cursor();
+
 
 int main()
 {
     char canvas[CANVAS_HEIGHT][CANVAS_WIDTH];
 
+    Tree tree1 = {0, 0, 7, '.'};
+    Tree tree2 = {11, 2, 6, '.'};
+    Tree tree3 = {3, 7, 10, '.'};
+
     printf("Merry Christmas!\n\n");
 
     canvas_wipe(canvas);
-    canvas_write_tree(canvas, 0, 0, 7, '.');
-    canvas_write_tree(canvas, 11, 2, 6, '@');
-    canvas_write_tree(canvas, 3, 7, 10, '*');
+    canvas_write_tree(canvas, tree1);
+    canvas_write_tree(canvas, tree2);
+    canvas_write_tree(canvas, tree3);
     canvas_print(canvas);
     reset_cursor();
     canvas_print(canvas);
@@ -73,19 +88,19 @@ void canvas_write(char canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int rel_row, int rel
     }
 }
 
-void canvas_write_tree(char canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int x, int y, int height, char leaf_char)
+void canvas_write_tree(char canvas[CANVAS_HEIGHT][CANVAS_WIDTH], Tree tree)
 {
-    int width = 2 * height - 1;
+    int width = 2 * tree.height - 1;
 
     int rel_row = 0;
 
-    for (rel_row = 0; rel_row < height; rel_row++)
+    for (rel_row = 0; rel_row < tree.height; rel_row++)
     {
         int tree_pixels = 2 * rel_row + 1;
-        int rel_col = height - rel_row - 1;
+        int rel_col = tree.height - rel_row - 1;
         for (int j = 0; j < tree_pixels; j++, rel_col++)
         {
-            canvas_write(canvas, rel_row + y, rel_col + x, leaf_char);
+            canvas_write(canvas, rel_row + tree.y, rel_col + tree.x, tree.leaf_char);
         }
     }
 
@@ -95,24 +110,24 @@ void canvas_write_tree(char canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int x, int y, i
 
     int rel_col = start_of_trunk - 1;
 
-    canvas_write(canvas, rel_row + y, rel_col + x, '|');
+    canvas_write(canvas, rel_row + tree.y, rel_col + tree.x, '|');
     rel_col++;
 
     for (int i = start_of_trunk; i < end_of_trunk; i++, rel_col++)
     {
-        canvas_write(canvas, rel_row + y, rel_col + x, ' ');
+        canvas_write(canvas, rel_row + tree.y, rel_col + tree.x, ' ');
     }
-    canvas_write(canvas, rel_row + y, rel_col + x, '|');
+    canvas_write(canvas, rel_row + tree.y, rel_col + tree.x, '|');
     rel_col++;
     rel_row++;
 
     rel_col = start_of_trunk - 1 - 1;
-    canvas_write(canvas, rel_row + y, rel_col + x, '[');
+    canvas_write(canvas, rel_row + tree.y, rel_col + tree.x, '[');
     rel_col++;
     for (int i = start_of_trunk - 1; i < end_of_trunk + 1; i++, rel_col++)
     {
-        canvas_write(canvas, rel_row + y, rel_col + x, '_');
+        canvas_write(canvas, rel_row + tree.y, rel_col + tree.x, '_');
     }
-    canvas_write(canvas, rel_row + y, rel_col + x, ']');
+    canvas_write(canvas, rel_row + tree.y, rel_col + tree.x, ']');
     rel_col++;
 }
