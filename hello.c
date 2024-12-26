e2cca4987fa6c6cf79b5fc7ad0cd2f8dbc7ded2a
John Ryan <jtigger@pm.me>
2024-12-25 17:43:46 -0800

    🎉Hardcoded xmas tree

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    printf("Merry Christmas!\n");
+
+    printf("      *\n");
+    printf("     ***\n");
+    printf("    *****\n");
+    printf("   *******\n");
+    printf("  *********\n");
+    printf(" ***********\n");
+    printf("*************\n");
+    printf("     | |\n");
+    printf("    [___]\n");
+    return 0;
+}
