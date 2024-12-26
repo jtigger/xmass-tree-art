d317408b8ec424636a7d3af40344110ee4c6fc76
John Ryan <jtigger@pm.me>
2024-12-26 09:55:38 -0800

    ✨Extract leaf character from canvas_write_tree()
    ... so that we can print trees of different "color"

--- hello.c
+++ hello.c
@@ -13,7 +13,7 @@ void canvas_wipe(char canvas[CANVAS_HEIGHT][CANVAS_WIDTH]);
 void canvas_write(char canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int rel_row, int rel_col, char c);
 
 // Write a tree to the canvas at the given height.
-void canvas_write_tree(char canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int x, int y, int height);
+void canvas_write_tree(char canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int x, int y, int height, char leaf_char);
 
 int main()
 {
@@ -22,7 +22,7 @@ int main()
     printf("Merry Christmas!\n");
 
     canvas_wipe(canvas);
-    canvas_write_tree(canvas, 0, 0, 7);
+    canvas_write_tree(canvas, 0, 0, 7, '*');
     canvas_print(canvas);
 
     return 0;
@@ -59,7 +59,7 @@ void canvas_write(char canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int rel_row, int rel
     }
 }
 
-void canvas_write_tree(char canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int x, int y, int height)
+void canvas_write_tree(char canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int x, int y, int height, char leaf_char)
 {
     int width = 2 * height - 1;
 
@@ -71,7 +71,7 @@ void canvas_write_tree(char canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int x, int y, i
         int rel_col = height - rel_row - 1;
         for (int j = 0; j < tree_pixels; j++, rel_col++)
         {
-            canvas_write(canvas, rel_row + y, rel_col + x, '*');
+            canvas_write(canvas, rel_row + y, rel_col + x, leaf_char);
         }
     }
 
