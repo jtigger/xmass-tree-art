e9128aa7871b090bc40b590bfbf8351b735dd22f
John Ryan <jtigger@pm.me>
2024-12-26 08:38:25 -0800

    ♻️(write to scan line) trunk and pot

--- hello.c
+++ hello.c
@@ -17,14 +17,14 @@ int main() {
 
 
 
-    const int CANVAS_HEIGHT = 12;
-    const int CANVAS_WIDTH = 14;
+    const int CANVAS_HEIGHT = 9;
+    const int CANVAS_WIDTH = 13;
 
     char canvas[CANVAS_HEIGHT][CANVAS_WIDTH];
 
     for (int i = 0; i < CANVAS_HEIGHT; i++) {
         for (int j = 0; j < CANVAS_WIDTH; j++) {
-            canvas[i][j] = '.';
+            canvas[i][j] = ' ';
         }
     }
 
@@ -41,28 +41,28 @@ int main() {
             canvas[scan_row][scan_col] = '*';
         }
     }
+
     int start_of_trunk = (width - 1) / 2;
     int thickness_of_trunk = 1;
     int end_of_trunk = start_of_trunk + thickness_of_trunk;
 
-    for (int i = 1; i < start_of_trunk; i++) {
-        printf(" ");
-    }
-    printf("|");
-    for (int i = start_of_trunk; i < end_of_trunk; i++) {
-        printf(" ");
-    }
-    printf("|\n");
-    for (int i = 1; i < start_of_trunk-1; i++) {
-        printf(" ");
-    }
-    printf("[");
-    for (int i = start_of_trunk-1; i < end_of_trunk+1; i++) {
-        printf("_");
-    }
-    printf("]\n");
+    int scan_col = 0;
+    scan_col = start_of_trunk-1;
 
-    printf("\n");
+    canvas[scan_row][scan_col++] = '|';
+
+    for (int i = start_of_trunk; i < end_of_trunk; i++, scan_col++) {
+        canvas[scan_row][scan_col] = ' ';
+    }
+    canvas[scan_row][scan_col++] = '|';
+    scan_row++;
+
+    scan_col = start_of_trunk-1-1;
+    canvas[scan_row][scan_col++] = '[';
+    for (int i = start_of_trunk-1; i < end_of_trunk+1; i++, scan_col++) {
+        canvas[scan_row][scan_col] = '_';
+    }
+    canvas[scan_row][scan_col++] = ']';
 
     for (int i = 0; i < CANVAS_HEIGHT; i++) {
         for (int j = 0; j < CANVAS_WIDTH; j++) {
