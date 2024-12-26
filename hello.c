71feb5602f1bf1c694d2842db67df4f5393a66bb
John Ryan <jtigger@pm.me>
2024-12-26 08:25:31 -0800

    ♻️(write to scan line) top of tree
    ... separating the concern of building the tree image from displaying it
    on the screen.

--- hello.c
+++ hello.c
@@ -16,18 +16,30 @@ int main() {
     printf("\n-----\n\n");
 
 
-    int height = 14;
+
+    const int CANVAS_HEIGHT = 12;
+    const int CANVAS_WIDTH = 14;
+
+    char canvas[CANVAS_HEIGHT][CANVAS_WIDTH];
+
+    for (int i = 0; i < CANVAS_HEIGHT; i++) {
+        for (int j = 0; j < CANVAS_WIDTH; j++) {
+            canvas[i][j] = '.';
+        }
+    }
+
+    int height = 7;
     int width = 2 * height - 1;
 
-    for (int row = 0; row < height; row++) {
-        int leaves = 2 * row + 1;
-        for (int air = 0; air < height - row - 1; air++) {
-            printf(" ");
+    int scan_row=0;
+
+    for (int row = 0; row < height; row++, scan_row++) {
+        int tree_pixels = 2 * row + 1;
+        int bg_pixels = 0;
+        int scan_col = height - row - 1;
+        for (int j = 0; j < tree_pixels; j++, scan_col++) {
+            canvas[scan_row][scan_col] = '*';
         }
-        for (int j = 0; j < leaves; j++) {
-            printf("*");
-        }
-        printf("\n");
     }
     int start_of_trunk = (width - 1) / 2;
     int thickness_of_trunk = 1;
@@ -52,6 +64,12 @@ int main() {
 
     printf("\n");
 
+    for (int i = 0; i < CANVAS_HEIGHT; i++) {
+        for (int j = 0; j < CANVAS_WIDTH; j++) {
+            printf("%c", canvas[i][j]);
+        }
+        printf("\n");
+    }
     
     return 0;
 }
